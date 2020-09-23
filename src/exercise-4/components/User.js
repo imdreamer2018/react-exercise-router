610bda1b3d62e975de86b67bb779c88f850e8c60
imdreamer <743295483@qq.com>
2020-09-24 00:06:45 +0800

    [yangqian] complete exrrcise-4.

--- src/exercise-4/components/User.js
+++ src/exercise-4/components/User.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import NotMatch from "./NotMatch";
 
-const User = () => {
-  return (
-    <div>
-      User profile page.
-    </div>
-  );
+const User = (props) => {
+  console.log(props.match.params.user);
+  console.log(isNaN(props.match.params.user));
+  if(!isNaN(props.match.params.user)) {
+    return (
+      <div>
+        User profile page.
+      </div>
+    );
+  }else {
+    return <NotMatch/>
+  }
+
+
 };
 
 export default User;
